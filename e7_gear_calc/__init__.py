703f4ee6be6f8607a5a871570299f9f5639221cf
dj5822 <dj5822@gmail.com>
2022-09-13 12:04:52 +1200

    Use tesseract to do OCR

--- e7_gear_calc/__init__.py
+++ e7_gear_calc/__init__.py
@@ -1,7 +1,26 @@
-import pyautogui
+from PIL import Image
+import pytesseract
+import cv2
+import numpy as np
 
 def main():
-    screenWidth, screenHeight = pyautogui.size()
-    print(screenWidth)
-    print(screenHeight)
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
+    gearImage = cv2.imread('testImage.png')
+
+    hsv = cv2.cvtColor(gearImage, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([0, 0, 60])
+    upper = np.array([0, 0, 255])
+
+    result = cv2.inRange(hsv, lower, upper)
+
+    cv2.imshow('mask', result)
+    cv2.waitKey(0)
+
+
+    result = pytesseract.image_to_string(result)
+    
+    print(result)
+
+main()
